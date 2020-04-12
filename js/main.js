6f8a8f4d9db423a10f1381b0237df4f028802436
Machulyanskiy <machulyanskiy.dmytro.wsk@gmail.com>
2020-04-12 08:56:02 +0300

    Change nav/offsets/partners + fix video

--- js/main.js
+++ js/main.js
@@ -21,24 +21,44 @@ $(function() {
 	$(window).scroll(() => {
 		rotation++;
 		rotationStr = `rotate(${rotation}deg)`;
-		console.log(rotationStr);
+		// console.log(rotationStr);
 		rider.css({
 			"-webkit-transform": rotationStr,
 			"-moz-transform": rotationStr,
 			"transform": rotationStr
 		});
 	});
+
+	const navHeight = $('#nav-panel').height();
 	
-	/* const navbarCollapse = function() {
-		console.log(window.scrollY);
-		if (window.scrollY > $('#description').offset().top) {
-		  $("#mainNav").show();
+	const navbarCollapse = function() {
+		// console.log(window.scrollY);
+		if (window.scrollY > navHeight) {
+		  	$("#main-nav").addClass('main-nav-tiny');
 		} else {
-		  $("#mainNav").hide();
+			$("#main-nav").removeClass('main-nav-tiny');
 		}
 	};
 	// Collapse now if page is not at top
 	navbarCollapse();
 	// Collapse the navbar when page is scrolled
-	$(window).scroll(navbarCollapse); */
+	$(window).scroll(navbarCollapse);
+
+	/* console.log($('#slick-test'));
+	$('#slick-test').slick({
+		slidesToScroll: 1,
+		dots: true,
+		centerMode: true
+	  }); */
 });
+
+function scrollToSection(sectionId) {
+	const element = $(sectionId);
+	const headerHeight = sectionId === '#page-top' ? 0 : $('#main-nav').height();
+	const offsetPosition = element[0].offsetTop - headerHeight;
+	
+	window.scrollTo({
+		top: offsetPosition,
+		behavior: "smooth"
+	});
+}
